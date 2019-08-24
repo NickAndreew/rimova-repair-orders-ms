4cd8525335ae866ef67efd201fc2dfd161515ee8
Mykyta Andreiev <nickandreev47@gmail.com>
2019-08-24 13:40:51 +0300

    git init

---
+++ src/main/java/com/rimowa/repairorders/RimovaRepairOrders/RimovaRepairOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.rimowa.repairorders.RimovaRepairOrders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimovaRepairOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimovaRepairOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rimowa/repairorders/RimovaRepairOrders/RimovaRepairOrdersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rimowa.repairorders.RimovaRepairOrders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RimovaRepairOrdersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
